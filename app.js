7a76affe7e8102951df06b2eb4a06ffeef4cca7a
Ezequiel <ezequielsale123@gmail.com>
2023-01-19 18:15:45 -0300

    Se coloco el boton borrar con funcionalidad de borrar tarea

--- app.js
+++ app.js
@@ -1,44 +1,88 @@
-const dateNumber = document.getElementById('dateNumber')
-const dateText = document.getElementById('dateText')
-const dateMonth = document.getElementById('dateMonth')
-const dateYear = document.getElementById('dateYear')
-const tasksContainer = document.getElementById('tasksContainer')
+const dateNumber = document.getElementById("dateNumber");
+const dateText = document.getElementById("dateText");
+const dateMonth = document.getElementById("dateMonth");
+const dateYear = document.getElementById("dateYear");
+const tasksContainer = document.getElementById("tasksContainer");
+const taskInput = document.getElementById("taskText");
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 const setDate = () => {
-    const date = new Date()
-    dateNumber.textContent = date.toLocaleString('es', {day: 'numeric'})
-    dateText.textContent = date.toLocaleString('es', {weekday: 'long'})
-    dateMonth.textContent = date.toLocaleString('es', {month: 'short'})
-    dateYear.textContent = date.toLocaleString('es', {year: 'numeric'})
-}
+  const date = new Date();
+  dateNumber.textContent = date.toLocaleString("es", { day: "numeric" });
+  dateText.textContent = date.toLocaleString("es", { weekday: "long" });
+  dateMonth.textContent = date.toLocaleString("es", { month: "short" });
+  dateYear.textContent = date.toLocaleString("es", { year: "numeric" });
+};
 
 const addNewTask = (event) => {
-    event.preventDefault()
-    const { value } = event.target.taskText
-    if (!value) return
-    const task = document.createElement('div')
-    task.classList.add('task', 'roundBorder')
-    task.addEventListener('click', changeTaskState)
-    task.textContent = value
-    tasksContainer.prepend(task)
-    event.target.reset()
+  event.preventDefault();
+  const task = taskText.value;
+  const taskObj = {
+    task,
+    id: Date.now()
+  };
+  tasks = [...tasks, taskObj];
+  createTask();
+  event.target.reset();
+};
+
+function createTask() {
+  clearHTML();
+  if (tasks.length > 0) {
+    tasks.forEach((task) => {
+      const div = document.createElement("div");
+      div.addEventListener("click", changeTaskState);
+      div.classList.add("task", "roundBorder");
+      div.innerHTML = `${task.task} <button task-id="${task.id}" class="botonBorrar">X</button>`;
+      tasksContainer.prepend(div);
+    });
+  }
+  syncLocalStorage();
+}
+
+function syncLocalStorage(){
+    localStorage.setItem("tasks", JSON.stringify(tasks)); 
+}
+
+function clearHTML() {
+  tasksContainer.innerHTML = "";
 }
 
 const changeTaskState = (event) => {
-    event.target.classList.toggle('done')
-}
+  event.target.classList.toggle("done");
+}; 
 
 const order = () => {
-    const done = []
-    const toDo = []
-    tasksContainer.childNodes.forEach( el => {
-        el.classList.contains('done') ? done.push(el) : toDo.push(el)
-    })
-    return [...toDo, ...done]
-}
+  const done = [];
+  const toDo = [];
+  tasksContainer.childNodes.forEach((el) => {
+    el.classList.contains("done") ? done.push(el) : toDo.push(el);
+  });
+  return [...toDo, ...done];
+};
 
 const orderedTask = () => {
-    order().forEach( el => tasksContainer.appendChild(el))
-} 
+  order().forEach((el) => tasksContainer.appendChild(el));
+};
 
-setDate()
+function initialCharge() {
+    if (tasks.length > 0) {
+        tasks.map((task) =>{
+            createTask(task);
+        })
+    }
+}
+
+initialCharge();
+
+setDate();
+
+tasksContainer.addEventListener('click', deleteTask)
+
+function deleteTask(e) {
+  if (e.target.className == 'botonBorrar') {
+      const deleteId = parseInt(e.target.getAttribute('task-id'));
+      tasks = tasks.filter(task => task.id !== deleteId);
+      createTask()
+  }
+}
